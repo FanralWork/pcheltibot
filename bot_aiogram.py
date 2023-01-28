2bcb2715088b1ec79398b2a118ec30078789038c
Fanral_Python <dmitry.fomichev@bk.ru>
2023-01-28 18:01:36 +0300

    aiogram

--- bot_aiogram.py
+++ bot_aiogram.py
@@ -6,10 +6,14 @@ token = config.TOKEN
 bot = Bot(token)
 dp = Dispatcher(bot=bot)
 
+async def on_startup(_):
+    print("Bot is active!")
+
 @dp.message_handler(commands=["start"])
 async def send_message(msg: types.Message):
     bot_name = await bot.get_me()
     await msg.answer(f"Приветствую, {msg.from_user.full_name}!\nЯ - <b>{bot_name.first_name}</b>, развлекательно-новостной бот.", parse_mode="html")
+    await bot.send_sticker(msg.from_user.id, sticker="CAACAgIAAxkBAAEHfLhj1TfnDTXgju-hIIhQ7ssUdAZAdAACwRIAAvXQth0OkELw6I25My0E")
     await msg.delete()
 
 @dp.message_handler()
@@ -17,4 +21,4 @@ async def send_echo(msg: types.Message):
     await msg.reply(msg.text)
 
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    executor.start_polling(dp, on_startup=on_startup)
